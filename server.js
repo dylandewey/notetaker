2749fec2b91b10fcacad04800b4c81766c9da4cf
Dylan Dewey <dylandewey41@gmail.com>
2020-08-04 16:48:06 -0600

    installed dependencies and tested the app listener

--- server.js
+++ server.js
@@ -2,12 +2,21 @@ const express = require('express');
 const fs = require('fs');
 const path = require('path');
 
+// creating an 'express' server
 const app = express();
-const port = 7500;
 
+//Initial PORT
+const PORT = 7500;
+
+// Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+//Routes File
+// require('./routs/apiRoutes')(app);
+// require('./routes/htmlRoutes')(app);
+
+//Starts the server
 app.listen(PORT, function () {
     console.log('App listening on PORT ' + PORT);
 });
