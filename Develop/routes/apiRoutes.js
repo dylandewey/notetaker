f9c1dd68b47494eb1b7aa6c92dfaa513641b3443
Dylan Dewey <dylandewey41@gmail.com>
2020-08-04 18:21:03 -0600

    adding apiRoutes

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -2,12 +2,27 @@ const express = require('express').Router();
 
 module.exports = function (app) {
 
-    app.get('/api/notes', function (req, res) {
-        res.json(notes);
-    })
-
-    fs.readFile('db/db.json', (err, data) {
+    fs.readFile('db/db.json', function (err, data) {
         if (err) throw err;
+        let notes = JSON.parse(data);
+
+        app.get('/api/notes', function (req, res) {
+            res.json(notes);
+        });
+
+        app.post('/api/notes', function (req, res) {
+            let newNote = req.body;
+            notes.push(newNote);
+            updateDb();
+            return console.log('Added new note: "+newNote.title');
+        });
+
+        app.get('/api/notes/:id', function (req, res) {
+            notes.splice(req.params.id, 1);
+            updateDb();
+            console.log('Deleted note with id " +req.params.id');
+        });
+
 
     });
 }
