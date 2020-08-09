e88f21ba63eb6edfd70ac7b7c20446909719a7df
dylandewey <dylandewey41@gmail.com>
2020-08-08 19:52:53 -0600

    changed port

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require('express');
 const app = express();
 
 //Initial PORT
-const PORT = process.env.PORT || 7500;
+const PORT = process.env.PORT || 3001;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
