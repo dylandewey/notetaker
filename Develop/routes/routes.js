d5c2f4080fbd3c357881f3c1e05e8aa1cc6d57fd
Dylan Dewey <dylandewey41@gmail.com>
2020-08-10 14:39:57 -0600

    trying to get changes to showup on the screen

--- Develop/routes/routes.js
+++ Develop/routes/routes.js
@@ -10,45 +10,57 @@ module.exports = function (app) {
         app.get('/api/notes', function (req, res) {
             res.json(notes);
         });
-
-        app.post('/api/notes', function (req, res) {
+    })
+    app.post('/api/notes', function (req, res) {
+        fs.readFile('db/db.json', (err, data) => {
+            if (err) throw err;
             let newNote = req.body;
             notes.push(newNote);
-            updateDb();
-            return console.log('Added new note: ' + newNote.title);
-        });
+            //updateDb();
+        })
+        fs.writeFile('db/db.json', JSON.stringify(notes), err => {
+            if (err) throw err;
+            res.redirect('/notes');
+        })
+    });
 
-        app.get('/api/notes/:id', function (req, res) {
-            res.json(notes[req.params.id]);
-            return console.log('Retrieved note: ' + newNote.title);
-        });
+    app.get('/api/notes/:id', function (req, res) {
+        res.json(notes[req.params.id]);
+        return console.log('Retrieved note: ' + newNote.title);
+    });
 
-        app.delete('/api/notes/:id', function (req, res) {
-            notes.splice(req.params.id, 1);
-            updateDb();
-            console.log('Deleted note with id " +req.params.id');
-        });
+    app.delete('/api/notes/:id', function (req, res) {
+        notes.splice(req.params.id, 1);
+        updateDb();
+        console.log('Deleted note with id " +req.params.id');
+        res.redirect('/notes');
+    });
 
-        app.get('/notes', function (req, res) {
-            res.sendFile(path.join(__dirname, '../public/notes.html'));
-        });
+    app.get('/notes', function (req, res) {
+        fs.readFile("db/db.json", "utf-8", (err, data) => {
+            return err ? console.log(err) : res.json(JSON.parse(data))
+        }
+            // res.sendFile(path.join(__dirname, '../public/notes.html'));
+            // }
+        );
 
         app.get("/assets/js/index.js", function (req, res) {
             res.sendFile(path.join(__dirname, "../public/assets/js/index.js"));
-          });
-          
-          app.get("/assets/css/styles.css", function (req, res) {
+        });
+
+        app.get("/assets/css/styles.css", function (req, res) {
             res.sendFile(path.join(__dirname, "../public/assets/css/styles.css"));
-          });
+        });
 
         app.get('*', function (req, res) {
             res.sendFile(path.join(__dirname, '../public/index.html'));
         });
 
         function updateDb() {
-            fs.writeFile('Develop/db/db.json',JSON.stringify(notes,'\t'), function(err) {
+            fs.writeFile('Develop/db/db.json', JSON.stringify(notes, '\t'), function (err) {
                 if (err) throw err;
-                return true;
+                return res.redirect('/notes');
+                //return true;
             });
         }
     });
